2705d88c48f1a30b75adc65379df25f8420c5a1f
Adebayo Abiodun <email@example.com>
2024-07-08 12:55:56 +0100

    fixing build

--- src/app/api/depart/route.ts
+++ src/app/api/depart/route.ts
@@ -3,20 +3,20 @@ import prisma from "../../../../util/prisma";
 import { Session_tb, Department_tb } from "@prisma/client";
 
 export async function GET(request: Request) {
-  const getDepartment = await prisma.department_tb.findMany({
-    where: {
-      id: 4,
-    },
-    include: {
-      user: {
-        select: {
-          id: true,
-          email: true,
-          firstName: true,
-        },
-      },
-    },
-  });
+  // const getDepartment = await prisma.department_tb.findMany({
+  //   where: {
+  //     id: 4,
+  //   },
+  //   include: {
+  //     user: {
+  //       select: {
+  //         id: true,
+  //         email: true,
+  //         firstName: true,
+  //       },
+  //     },
+  //   },
+  // });
 
-  return NextResponse.json({ data: getDepartment });
+  return NextResponse.json({ data: "" });
 }
